b2decd7386214d8bdf621f8f17d1e07d733da703
izzy <israelrankin@gmail.com>
2025-08-11 07:00:59 +0000

    added home class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,10 @@
+import Home from "./pages/home";
+
 const hamburger = document.querySelector(".nav__hamburger");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
 });
+
+
+new Home()
